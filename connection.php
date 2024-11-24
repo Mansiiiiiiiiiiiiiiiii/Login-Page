78793d099ec276b2c324dbc2f2062def7dddcd77
Mansiiiiiiiiiiiiiiiii <mansi15094@gmail.com>
2024-11-24 19:51:03 +0530

    Login Page

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "M@n$!kum12";
+$dbname = "responsiveform3";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if ($conn){
+  
+}
+else{
+    echo "Connection failed";
+}
+?>
