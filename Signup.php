cbda9eb347103b0a4d869546781656b2b1803f84
Mansiiiiiiiiiiiiiiiii <mansi15094@gmail.com>
2024-11-24 19:50:46 +0530

    Login Page

---
+++ Signup.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="slidestyle.css">
+</head>
+<body>
+    <div class="main">
+        <input type="checkbox" id="chk" aria-hidden="true">
+        <div class="signup">
+            <form action="">
+                <label for="chk" aria-hidden="true">Sign Up</label>
+                <input type="text" name="txt" placeholder="User name" required>
+                <input type="email" name="email" placeholder="Email" required>
+                <input type="Password" name="pswd" placeholder="Password" required>
+                <button type="submit">Sign up</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
